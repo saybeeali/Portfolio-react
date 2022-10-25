a8e61fe3799bb1aa24c89d2f0f8202b3dcce04cc
Sohaib Ali <saybe.ali0618@gmail.com>
2022-10-25 12:24:32 -0400

    finished designing projects

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -15,15 +15,15 @@ function Projects(){
         </div>
       </div>
     </section>
-    <section id="parallax-1" class="hero is-large ">
+    <section id="parallax-1" class="hero is-medium ">
       <div class="hero-body">
         <div class="container">
           <div class="columns">
-            <div class="column is-6 is-offset-6">
-              <h1 class="bravado is-1 ">Bravado</h1>
+            <div class="column is-7 is-offset-6">
+              <h1 class="bravado-title is-1 ">Bravado</h1>
               <hr class="content-divider"/>
               <h2 class="bravado">A fullstack web application where people share interview experience as well as knowledge about the interview process that will allow others to study or use as reference for how to ace their own upcoming interviews. </h2>
-               <a href="#" class="button is-white is-inverted">Next&ensp;<i class="fad fa-chevron-right"></i></a>
+               <a href="https://github.com/javenegas1/Bravado" class="button is-white is-inverted">Github and Website here!&ensp;<i class="fad fa-chevron-right"></i></a>
             </div>
           </div>
         </div>
@@ -33,11 +33,11 @@ function Projects(){
       <div class="hero-body">
         <div class="container">
           <div class="columns">
-            <div class="column is-6">
-              <h1 class="title is-1 ">AnimeDB</h1>
+            <div class="column is-4">
+              <h1 class="anime-title is-1 ">AnimeDB</h1>
               <hr class="content-divider"/>
-              <p class="subtitle">AnimeDB is a site where anime fans can come and find information on all the top anime, watch trailers and post and read reviews.</p>
-               <a href="#" class="button is-white is-inverted">Next&ensp;<i class="fad fa-chevron-right"></i></a> 
+              <p class="anime">AnimeDB is a site where anime fans can come and find information on all the top anime, watch trailers and post and read reviews.</p>
+               <a href="https://github.com/saybeeali/animedb-front-end" class="button is-white is-inverted">Github and Website here!&ensp;<i class="fad fa-chevron-right"></i></a> 
             </div>
           </div>
         </div>
